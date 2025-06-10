eaf3a8774eeb9d03b8e0c38c589fad827161cc00
mukund <mukundt513@gmail.com>
2025-06-10 22:35:58 +0530

    init backend

---
+++ apps/api/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get("website",(req,res) =>{
+
+})
+
+app.get("status/websiteId",(req,res) =>{
+
+})
+
+app.listen(3000,() =>{
+    console.log(`Server is running on port ${3000}`)
+})
